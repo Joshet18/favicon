f1147a61e5fa068bf6baababae13144febad40ac
Joshet18 <74809921+Joshet18@users.noreply.github.com>
2023-05-29 04:33:36 -0500

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+if (( $EUID != 0 )); then
+    echo "Please run as root"
+    exit
+fi
+
+clear
+
+install(){
+    cd /var/www/
+    tar -cvf BackUP.tar.gz pterodactyl
+    echo "Installing favicon..."
+    cd /var/www/pterodactyl/public/favicons
+    unlink apple-touch-icon.png
+    git clone https://github.com/Angelillo15/MinecraftPurpleTheme.git
+    unlink run.sh
+    echo "Favicon installed"
+}
+echo "[1] Install Favicon"
+echo "[2] Exit"
+
+read -p "Please enter a number: " choice
+if [ $choice == "1" ]
+    then
+    install
+fi
+if [ $choice == "2" ]
+    then
+    exit
+fi
